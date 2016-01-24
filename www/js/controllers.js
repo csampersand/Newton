3e30032c5a0cb668cdb6d002f663a244e1d50053
Chris Anderson <chris.scott.anders@gmail.com>
2016-01-23 23:58:46 -0500

    Cleaning

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,12 +1,33 @@
 angular.module('tasks.list', [])
 
 .controller('TasksCtrl', function($scope) {
-  $scope.tasks = [
-    { id: 1, title: 'Task #', due: 'Jan 5', complete: 0 },
-    { id: 2, title: 'Task #', due: 'Jan 5', complete: 0 },
-    { id: 3, title: 'Task #', due: 'Jan 5', complete: 0 },
-    { id: 4, title: 'Task #', due: 'Jan 5', complete: 0 },
-    { id: 5, title: 'Task #', due: 'Jan 5', complete: 0 },
-    { id: 6, title: 'Task #', due: 'Jan 5', complete: 0 }
-  ];
-})
+  $scope.tasks = [];
+
+
+  // // Create and load the Modal
+  // $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
+  //   $scope.taskModal = modal;
+  // }, {
+  //   scope: $scope,
+  //   animation: 'slide-in-up'
+  // });
+
+  // // Called when the form is submitted
+  // $scope.createTask = function(task) {
+  //   $scope.tasks.push({
+  //     title: task.title
+  //   });
+  //   $scope.taskModal.hide();
+  //   task.title = "";
+  // };
+
+  // // Open our new task modal
+  // $scope.newTask = function() {
+  //   $scope.taskModal.show();
+  // };
+
+  // // Close the new task modal
+  // $scope.closeNewTask = function() {
+  //   $scope.taskModal.hide();
+  // };
+});
