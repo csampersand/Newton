b8a8b2f84a7f937df016579dad6901be26d1f198
Chris Anderson <chris.scott.anders@gmail.com>
2016-01-23 22:35:04 -0500

    Added some variables and css

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -2,11 +2,11 @@ angular.module('tasks.list', [])
 
 .controller('TasksCtrl', function($scope) {
   $scope.tasks = [
-    { title: 'Task #', due: 'Jan 5' },
-    { title: 'Task #', due: 'Jan 5' },
-    { title: 'Task #', due: 'Jan 5' },
-    { title: 'Task #', due: 'Jan 5' },
-    { title: 'Task #', due: 'Jan 5' },
-    { title: 'Task #', due: 'Jan 5' }
+    { id: 1, title: 'Task #', due: 'Jan 5', complete: 0 },
+    { id: 2, title: 'Task #', due: 'Jan 5', complete: 0 },
+    { id: 3, title: 'Task #', due: 'Jan 5', complete: 0 },
+    { id: 4, title: 'Task #', due: 'Jan 5', complete: 0 },
+    { id: 5, title: 'Task #', due: 'Jan 5', complete: 0 },
+    { id: 6, title: 'Task #', due: 'Jan 5', complete: 0 }
   ];
 })
