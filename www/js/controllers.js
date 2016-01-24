ac2d10c6899c350003303bf3084eb2a162463db1
acoverstone <acoverstone@ufl.edu>
2016-01-24 02:42:50 -0600

    Hopefully this works

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,33 +1,18 @@
 angular.module('tasks.list', [])
 
 .controller('TasksCtrl', function($scope) {
-  $scope.tasks = [];
+  $scope.tasks = [
+    {'id': 0, 'due': 'Monday', 'title': 'Assignment', 'complete': false, 'important': true}
+  ];
+
+  $scope.toggleComplete = function(index) {
+    
+    if($scope.tasks[index].complete == true)
+      $scope.tasks[index].complete = false;
+    else
+       $scope.tasks[index].complete = true;
+
+  }
 
 
-  // // Create and load the Modal
-  // $ionicModal.fromTemplateUrl('new-task.html', function(modal) {
-  //   $scope.taskModal = modal;
-  // }, {
-  //   scope: $scope,
-  //   animation: 'slide-in-up'
-  // });
-
-  // // Called when the form is submitted
-  // $scope.createTask = function(task) {
-  //   $scope.tasks.push({
-  //     title: task.title
-  //   });
-  //   $scope.taskModal.hide();
-  //   task.title = "";
-  // };
-
-  // // Open our new task modal
-  // $scope.newTask = function() {
-  //   $scope.taskModal.show();
-  // };
-
-  // // Close the new task modal
-  // $scope.closeNewTask = function() {
-  //   $scope.taskModal.hide();
-  // };
 });
